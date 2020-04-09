5e5978677ed8cea628c79bce6fc10ffd362b76ce
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-04-08 17:27:21 -0700

    updated title

--- client/src/components/News/news.tsx
+++ client/src/components/News/news.tsx
@@ -4,7 +4,15 @@ import './news.scss';
 const News: React.FC = () => {
     return (
         <section className="NEWS">
-            
+            <div className="updates">
+            <h1>
+                News
+            </h1>
+            <div className="newsheader">
+                
+            </div>
+
+            </div>
         </section>
      );
 };
