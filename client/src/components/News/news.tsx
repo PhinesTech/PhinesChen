0de97c5573379aca1e9e9c91c5b933732718f801
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-04-08 18:44:19 -0700

    rearrange news content

--- client/src/components/News/news.tsx
+++ client/src/components/News/news.tsx
@@ -13,51 +13,64 @@ const News: React.FC = () => {
             </h1>
             </div>
             <br/>
-            <div className="newsheader">
+            <div className="newsbox firstbox">
+            <div className="newsheader ">
                 New Program Aims to Help Families in need find
-                healthier <br/> fresh produce in Merced County
-                <p>
+                healthier fresh produce in Merced County
+                </div>
+            <div className="newscontent ">
+                <h3>
                 June 17, 2017
-                </p>
-                <p>
+                </h3>
+                <h3>
                 Health and food bank officials in Merced County are teaming up on a 
-                <br/>project to make fresh produce more accessible to food pantries and 
-                low-<br/>income  households. 
+                project to make fresh produce more accessible to food pantries and 
+                low-income households. 
 
-                The Merced County Food Bank and the Merced County <br/> Public Health Department 
-                are launching the Glean Grow Give project this summer <br/> as a way to add 
-                healthier food to diets, said Bill Gibbs, executive director <br/>of the food bank.
-                </p>
+                The Merced County Food Bank and the Merced County  Public Health Department 
+                are launching the Glean Grow Give project this summer.
+                </h3>
+            </div>
             </div>
             <br/>
-            <div className="newsheader">
+            <div className="newsbox secondbox">
+            <div className="newsheader ">
                 Food Insecurity a Growing Problem in Merced County
-                <p>
+                </div>
+                <div className="newscontent">
+                <h3>
                 December 16, 2016
-                </p>
-                <p>
-                Local food pantries are seeing increased demand this holiday season, <br/>
+                </h3>
+                <h3>
+                Local food pantries are seeing increased demand this holiday season,
                 despite a spike in year-end charitable giving. 
 
-                Since 2014, the Merced County Food Bank <br/> has also served as an official distribution 
-                site for the state’s drought-relief food box <br/> program, handing out an average of 12,000 
+                Since 2014, the Merced County Food Bank has also served as an official distribution 
+                site for the state’s drought-relief food box program, handing out an average of 12,000 
                 boxes every month to those in need.
-                </p>
+                </h3>
+            </div>
             </div>
             <br/>
-            <div className="newsheader">
+            <div className="newsbox thirdbox">
+            <div className="newsheader ">
                 One story of need turned into help for hundreds of hungry seniors
-                <p>
+                </div>
+                <div className="newscontent">
+                <h3>
                 October 27, 2016
-                </p>
-                <p>
-                The Senior Brown Bag Program is a service for residents ages 55 and older, <br/> 
+                </h3>
+                <h3>
+                The Senior Brown Bag Program is a service for residents ages 55 and older, 
                 according to the current executive director for the Merced County Food Bank, 
-                Bill Gibbs. <br/> A bag filled with food is given out twice a month in 16 cities 
-                throughout <br/> Merced County and one site in Mariposa County.
-                </p>
+                Bill Gibbs. A bag filled with food is given out twice a month in 16 cities 
+                throughout Merced County and one site in Mariposa County.
+                </h3>
             </div>
-
+            </div>
+            <br/>
+            <br/>
+            <br/>
             
         </section>
      );
