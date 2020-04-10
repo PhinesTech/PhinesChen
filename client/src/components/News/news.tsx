02b0c7bff6a9d2fa1dffd1947c77ddc96299bbf9
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-04-10 00:19:00 -0700

    news images

--- client/src/components/News/news.tsx
+++ client/src/components/News/news.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import VEGGIES from "../../assets/images/vegies.jpg";
+// import BANNER from "../../assets/images/banner.jpg";
+import SENIOR from "../../assets/images/Brown-bag-31-1.jpg";
+// import HERO from "../../assets/images/food-drive-hero.jpg";
+import STRAWBERRIES from "../../assets/images/FINAL-Merced-County-Food-Bank (1).jpg";
+
 import './news.scss';
 
 const News: React.FC = () => {
@@ -12,10 +18,13 @@ const News: React.FC = () => {
             </div>
             <div className="newsbox firstbox">
                 <div className="newsheader ">
-                    New Program Aims to Help Families in need find healthier fresh produce in Merced County
+                    New Program Aims to Help Families in need find healthier produce
                 </div>
                 <div className="newscontent ">
                     June 17, 2017
+                    <span>
+                    <img src={VEGGIES} alt="Newspaper"></img>
+                    </span>
                     <h3>
                         Health and food bank officials in Merced County are teaming up on a project to make fresh
                         produce more accessible to food pantries and low-income households. The Merced County Food Bank
@@ -28,6 +37,9 @@ const News: React.FC = () => {
                 <div className="newsheader ">Food Insecurity a Growing Problem in Merced County</div>
                 <div className="newscontent">
                     December 16, 2016
+                    <span>
+                    <img src={STRAWBERRIES} alt="Newspaper"></img>
+                    </span>
                     <h3>
                         Local food pantries are seeing increased demand this holiday season, despite a spike in year-end
                         charitable giving. Since 2014, the Merced County Food Bank has also served as an official
@@ -40,6 +52,9 @@ const News: React.FC = () => {
                 <div className="newsheader ">One story of need turned into help for hundreds of hungry seniors</div>
                 <div className="newscontent">
                     October 27, 2016
+                    <span>
+                    <img src={SENIOR} alt="Newspaper"></img>
+                    </span>
                     <h3>
                         The Senior Brown Bag Program is a service for residents ages 55 and older, according to the
                         current executive director for the Merced County Food Bank, Bill Gibbs. A bag filled with food
