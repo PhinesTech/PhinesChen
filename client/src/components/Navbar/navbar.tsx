232aeda41b0d07b5af02118786cebdb535428da5
Adrian Darian <adarian@ucmerced.edu>
2020-04-10 18:19:03 -0700

    Added routing

--- client/src/components/Navbar/navbar.tsx
+++ client/src/components/Navbar/navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import MCFB from '../../assets/images/mcfb-updated-colors2-resized.png';
 import './navbar.scss';
 
@@ -65,12 +67,12 @@ const Navbar: React.FC = () => {
                         </a>
                     </li>
                     <li className="login mr-3 relative inset-y-0 right-0 top-auto left-auto hover:text-gray-400  bottom-auto flex items-center pr-2 sm:ml-6 sm:pr-0">
-                        <a
+                        <Link
                             className=" inline-block font-bold hover:border rounded hover:border-black hover:text-gray-400 hover:bg-green-900 py-2 px-3 text-gray-100 "
-                            href="#"
+                            to="login"
                         >
                             Login
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
