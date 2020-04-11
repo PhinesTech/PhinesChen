8ff486f5785df6afcc30415f1df1e0e5240c85e8
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-04-10 22:16:35 -0700

    added signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/login';
+import Signup from './pages/Signup/signup';
+
 import './App.scss';
 
 const App: React.FC = () => {
@@ -11,6 +13,7 @@ const App: React.FC = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
             </Switch>
         </Router>
     );
--- client/src/pages/Login/login.tsx
+++ client/src/pages/Login/login.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 import Navbar from '../../components/Navbar/navbar';
 import Footer from '../../components/Footer/footer';
 
@@ -12,7 +14,7 @@ const Login: React.FC = () => {
           <div className="LOGIN">
             <div className="login-div">
                 <div className="logo"></div>
-                <div className="title">Merced County Food Bank Login</div>
+                <div className="title">Login</div>
 
                 <div className="fields">
                     <div className="username">
@@ -22,7 +24,7 @@ const Login: React.FC = () => {
                                 d="M896 307.2h-819.2c-42.347 0-76.8 34.453-76.8 76.8v460.8c0 42.349 34.453 76.8 76.8 76.8h819.2c42.349 0 76.8-34.451 76.8-76.8v-460.8c0-42.347-34.451-76.8-76.8-76.8zM896 358.4c1.514 0 2.99 0.158 4.434 0.411l-385.632 257.090c-14.862 9.907-41.938 9.907-56.802 0l-385.634-257.090c1.443-0.253 2.92-0.411 4.434-0.411h819.2zM896 870.4h-819.2c-14.115 0-25.6-11.485-25.6-25.6v-438.566l378.4 252.267c15.925 10.618 36.363 15.925 56.8 15.925s40.877-5.307 56.802-15.925l378.398-252.267v438.566c0 14.115-11.485 25.6-25.6 25.6z"
                             ></path>
                         </svg>
-                        <input type="username" className="user-input" placeholder="username" />
+                        <input type="email" className="user-input" placeholder="email" />
                     </div>
                     <div className="password">
                         <svg fill="#999" viewBox="0 0 1024 1024">
@@ -36,7 +38,13 @@ const Login: React.FC = () => {
                 </div>
                 <button className="signin-button bg-yellow-500">Login</button>
                 <div className="link">
-                    <a href="#">Forgot password?</a> or <a href="#">Sign up</a>
+                    <a href="#">Forgot password?</a> or  
+                    <Link
+                    to="signup"
+                        >
+                          &nbsp;
+                             Sign Up
+                        </Link>
                 </div>
             </div>
             </div>
