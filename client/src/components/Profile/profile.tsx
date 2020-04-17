f460e464430999d50d5897e8ebf88b8cde1ea24e
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-04-17 15:56:53 -0700

    added donate form

---
+++ client/src/components/Profile/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Profile: React.FC = () => {
+    return (
+        <section className="ADMIN">
+            Hello darkness my old friend
+        </section>
+    );
+};
+
+export default Profile;
