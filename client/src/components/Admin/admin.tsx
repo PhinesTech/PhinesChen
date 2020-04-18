b49d8a1b97b5662e0e161c325bbd1a482358a8a0
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-04-18 00:25:51 -0700

    put back graph

--- client/src/components/Admin/admin.tsx
+++ client/src/components/Admin/admin.tsx
@@ -42,7 +42,54 @@ const Admin: React.FC = () => {
                                 fill="rgb(111,232,255)"
                             ></path>
                             <text transform="matrix(1,0,0,1,195,5)">+14%</text>
+        
                             {/* style="font-family:&amp;quot;Open Sans&amp;quot;;font-weight:700;font-size:12px;font-style:normal;fill:#6fe8ff;stroke:none;" */}
+                            <div className="stock">
+                                    <div className="stock-logo paperpillar">
+                                        <i className="fa fa-inverse fa-angle-double-up"></i>
+                                    </div>
+                                    <div className="stock-info">
+                                        <div className="stock-name">Requestors</div>
+                                        <div className="stock-fullname">Individuals</div>
+                                    </div>
+                                </div>
+                            </svg>
+                        </div>
+                        <div className="graph">
+                            <svg viewBox="0 0 250 60" width="250" height="90">
+                                <path
+                                    d="M 209.328 17.34 C 221.956 17.588 235.264 32.599 250 22.328"
+                                    fill="none"
+                                    vectorEffect="non-scaling-stroke"
+                                    troke-width="2"
+                                    stroke="rgb(243,243,250)"
+                                    strokeLinejoin="miter"
+                                    strokeLinecap="round"
+                                    strokeMiterlimit="3"
+                                ></path>
+                                <linearGradient id="_lgradient_2" x1="0%" y1="50%" x2="100%" y2="50%">
+                                    <stop offset="0%" stopOpacity="1"></stop>
+                                    {/* style="stop-color:rgb(248, 135, 129);" */}
+                                    <stop offset="100%" stopOpacity="1"></stop>
+                                    {/* style="stop-color:rgb(247, 198, 130);" */}
+                                </linearGradient>
+                                <path
+                                    d=" M 0 43.634 C 5.934 43.634 11.318 51.209 17.462 51.342 C 33.219 51.683 30.603 59.567 39.187 59.868 C 46.963 60.141 50.44 44.192 60.537 43.77 C 69.126 43.77 72.129 52.461 79.739 52.433 C 90.904 52.433 94.93 38.455 106.648 39.78 C 129.082 42.317 124.556 27.606 139.157 27.177 C 153.758 26.747 158.235 44.485 171.96 44.725 C 196.438 45.155 189.782 17.1 208.248 17.1"
+                                    fill="none"
+                                    vectorEffect="non-scaling-stroke"
+                                    troke-width="2"
+                                    stroke="url(#_lgradient_2)"
+                                    strokeLinejoin="miter"
+                                    strokeLinecap="round"
+                                    strokeMiterlimit="3"
+                                ></path>
+                                <path
+                                    d="M 206.649 17.218 C 206.649 15.739 207.85 14.538 209.328 14.538 C 210.807 14.538 212.008 15.739 212.008 17.218 C 212.008 18.696 210.807 19.897 209.328 19.897 C 207.85 19.897 206.649 18.696 206.649 17.218 Z"
+                                    fill="rgb(247, 198, 130)"
+                                ></path>
+                                <text transform="matrix(1,0,0,1,195,5)">+14%</text>
+                                {/* style="font-family:&amp;quot;Open Sans&amp;quot;;font-weight:700;font-size:12px;font-style:normal;fill:rgb(247, 198, 130);stroke:none;" */}
+                            </svg>
                             <div className="stock">
                                 <div className="stock-logo dandruft">
                                     <i className="fa fa-inverse fa-circle-thin"></i>
