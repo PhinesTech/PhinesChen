dc952c1abe0e5e4714f8a1efbfea4c2935f06bba
Adrian Darian <adarian@ucmerced.edu>
2020-04-18 23:57:23 -0700

    init request service

---
+++ api/src/services/request/request.controller.js
@@ -0,0 +1,98 @@
+const httpStatus = require("http-status");
+
+const service = require("./user.service");
+const { handler: errorHandler } = require("../../middlewares/error");
+
+/**
+ * Load food and append to req.
+ * @public
+ */
+exports.load = async (req, res, next, id) => {
+  try {
+    const food = await service.get(id);
+    req.locals = { food };
+    return next();
+  } catch (error) {
+    return errorHandler(error, req, res);
+  }
+};
+
+/**
+ * Get food
+ * @public
+ */
+exports.get = (req, res) => res.json(req.locals.food.transform());
+
+/**
+ * Get logged in food info
+ * @public
+ */
+exports.loggedIn = (req, res) => res.json(req.food.transform());
+
+/**
+ * Create new food
+ * @public
+ */
+exports.create = async (req, res, next) => {
+  try {
+    const response = await service.create(req.body);
+    return res.status(httpStatus.CREATED).json(response);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * Replace existing food
+ * @public
+ */
+exports.replace = async (req, res, next) => {
+  try {
+    const { food } = req.locals;
+    const response = await service.replace(food, req.body);
+    return res.json(response);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * Update existing food
+ * @public
+ */
+exports.update = async (req, res, next) => {
+  try {
+    const { food } = req.locals;
+    const response = await service.update(food, req.body);
+    return res.json(response);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * Get food list
+ * @public
+ */
+exports.list = async (req, res, next) => {
+  try {
+    const response = await service.list(req.query);
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Delete food
+ * @public
+ */
+exports.remove = async (req, res, next) => {
+  try {
+    const { food } = req.locals;
+    await service.remove(food);
+    res.status(httpStatus.NO_CONTENT).end();
+  } catch (error) {
+    next(error);
+  }
+};
