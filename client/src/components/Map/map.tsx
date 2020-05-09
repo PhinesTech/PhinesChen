3dd82d909b6e9208fbd3288ca5c5926b5ac658aa
Manjot Singh <msingh795617@gmail.com>
2020-05-08 23:35:07 -0700

    Added alot of the food locators

--- client/src/components/Map/map.tsx
+++ client/src/components/Map/map.tsx
@@ -17,13 +17,125 @@ class Map extends Component<MapProps> {
             zoom: zoom,
         });
 
-        map.on('move', () => {
-            const { lng, lat } = map.getCenter();
-
-            this.setState({
-                lng: lng.toFixed(4),
-                lat: lat.toFixed(4),
-                zoom: map.getZoom().toFixed(2),
+        map.on('load', function() {
+            map.addSource('points', {
+                type: 'geojson',
+                data: {
+                    type: 'FeatureCollection',
+                    features: [
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.48134, 37.30034],
+                            },
+                            properties: {
+                                title: 'Catholic Charities Diocese',
+                                icon: 'triangle',
+                            },
+                        },
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.853439, 37.058701],
+                            },
+                            properties: {
+                                title: 'Merced Salvation',
+                                icon: 'triangle',
+                            },
+                        },
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.50329438754778, 37.3185086],
+                            },
+                            properties: {
+                                title: 'Merced County Food Bank',
+                                icon: 'triangle',
+                            },
+                        },
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.85054305, 37.06178],
+                            },
+                            properties: {
+                                title: 'Bethel Community Church',
+                                icon: 'triangle',
+                            },
+                        },
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.48218860999472, 37.303889],
+                            },
+                            properties: {
+                                title: 'Merced Resucue Mission',
+                                icon: 'triangle',
+                            },
+                        },
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.5006445013744, 37.3011427],
+                            },
+                            properties: {
+                                title: 'Salvation Army Center',
+                                icon: 'triangle',
+                            },
+                        },
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.47811946200821, 37.29970525],
+                            },
+                            properties: {
+                                title: 'Saint Vincent De Paul',
+                                icon: 'triangle',
+                            },
+                        },
+                        {
+                            // feature for Mapbox DC
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [-120.496459, 37.297878],
+                            },
+                            properties: {
+                                title: 'Calvary Assembly of God',
+                                icon: 'triangle',
+                            },
+                        },
+                    ],
+                },
+            });
+            map.addLayer({
+                id: 'points',
+                type: 'symbol',
+                source: 'points',
+                layout: {
+                    // get the icon name from the source's "icon" property
+                    // concatenate the name to get an icon from the style's sprite sheet
+                    'icon-image': ['concat', ['get', 'icon'], '-15'],
+                    // get the title name from the source's "title" property
+                    'text-field': ['get', 'title'],
+                    'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                    'text-offset': [0, 0.6],
+                    'text-anchor': 'top',
+                },
             });
         });
     }
